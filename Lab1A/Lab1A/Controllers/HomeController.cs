c68b60966116501afea0dea775344a5374fd5d61
bomnamu77 <bomnamu@gmail.com>
2018-09-29 23:42:13 -0700

    Lab1 beta

---
+++ Lab1A/Lab1A/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab1A.Models;
+
+namespace Lab1A.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult History()
+        {
+            ViewData["Message"] = "A brief history of the community";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Contact()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Contact(Contact contact)
+        {
+            ViewData["Message"] = "Your contact page.";
+            if (ModelState.IsValid)
+            {
+                
+                return View("ContactInputs", contact);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+            
+        }
+
+        
+    }
+}
---
+++ empty/empty/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace empty.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
